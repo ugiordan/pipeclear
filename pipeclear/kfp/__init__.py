02c627189de5a08bdbb35a66ffb583e9103c32e0
Ugo Giordano <ugiordan@redhat.com>
2026-03-19 11:59:28 +0100

    feat: export PipeClearCompiler and validate from kfp module

--- pipeclear/kfp/__init__.py
+++ pipeclear/kfp/__init__.py
@@ -1,5 +1,7 @@
 """PipeClear KFP components for pipeline validation."""
 
 from pipeclear.kfp.component import preflight_check
+from pipeclear.kfp.compiler import PipeClearCompiler
+from pipeclear.kfp.decorator import validate
 
-__all__ = ['preflight_check']
+__all__ = ['preflight_check', 'PipeClearCompiler', 'validate']
